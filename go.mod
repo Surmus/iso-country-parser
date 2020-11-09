4f9f7e90addb8f61eecf741caa79d68ca4d21561
Meelis Pinka <surmus@gmail.com>
2020-11-09 19:43:04 +0200

    Fixed wrong country name in result
    If ISO country is preceded by country without ISO code, then the name of previous country was added to result

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/surmus/iso-country-parser
 go 1.15
 
 require (
+	github.com/dlclark/regexp2 v1.4.0
 	github.com/sirupsen/logrus v1.7.0
 	github.com/stretchr/testify v1.2.2
 	github.com/urfave/cli v1.22.4
-	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
-	golang.org/x/tools v0.0.0-20201031021630-582c62ec74d0 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
 )
--- internal/parser/page.go
+++ internal/parser/page.go
@@ -1,26 +1,26 @@
 package parser
 
 import (
+	"github.com/dlclark/regexp2"
 	"github.com/sirupsen/logrus"
 	"github.com/surmus/iso-country-parser/internal"
 	"github.com/surmus/iso-country-parser/internal/wiki"
-	"regexp"
 )
 
 // matches strings which start with 'id=SOMEID' and end with '[[ISO 3166-2:XX]]' 'XX' being alpha-2 country code
-const countriesTableRowPattern = `id=\s*(.|\n|\r)*?\[\[ISO 3166-2:[A-Z]{2}]]`
+const countriesTableRowPattern = `id=\s*((?!id=).|\n|\r)*?\[\[ISO 3166-2:[A-Z]{2}]]`
 
 // WikiPageParser converts data from Wiki page into countries list
 type WikiPageParser struct {
 	page                     *wiki.Page
 	countryRowParser         *countriesTableRowParser
-	countriesTableRowMatcher *regexp.Regexp
+	countriesTableRowMatcher *regexp2.Regexp
 }
 
 // NewWikiPageParser creates new parser for given Wiki page
 func NewWikiPageParser(page *wiki.Page) *WikiPageParser {
 	return &WikiPageParser{
-		countriesTableRowMatcher: regexp.MustCompile(countriesTableRowPattern),
+		countriesTableRowMatcher: regexp2.MustCompile(countriesTableRowPattern, regexp2.ECMAScript),
 		page:                     page,
 		countryRowParser:         newCountriesTableRowParser(),
 	}
@@ -28,15 +28,23 @@ func NewWikiPageParser(page *wiki.Page) *WikiPageParser {
 
 // Parse parses Wiki page contents into list of countries, if no countries are found from page empty slice is returned
 func (p *WikiPageParser) Parse() []*internal.Country {
-	var result []*internal.Country
 	logrus.Info("begin parsing wiki page content")
-	countryRows := p.countriesTableRowMatcher.FindAllString(p.page.Text, -1)
+	countryRowMatch, _ := p.countriesTableRowMatcher.FindStringMatch(p.page.Text)
 
-	for _, row := range countryRows {
-		if country := p.countryRowParser.parse(row); country != nil {
-			result = append(result, country)
-		}
+	results := p.appendCountryMatch(countryRowMatch, []*internal.Country{})
+	logrus.Infof("found %d countries from wiki page", len(results))
+	return results
+}
+
+func (p *WikiPageParser) appendCountryMatch(match *regexp2.Match, results []*internal.Country) []*internal.Country {
+	if match == nil {
+		return results
 	}
-	logrus.Infof("found %d countries from wiki page", len(result))
-	return result
+
+	if country := p.countryRowParser.parse(match.String()); country != nil {
+		results = append(results, country)
+	}
+
+	nextMatch, _ := p.countriesTableRowMatcher.FindNextMatch(match)
+	return p.appendCountryMatch(nextMatch, results)
 }
